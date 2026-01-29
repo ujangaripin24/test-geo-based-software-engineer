6a6f6a98ffa4921547ae6f8c9be0e1156aface02
ujangaripin24 <ujangarifin06@gmail.com>
2026-01-29 13:29:42 +0700

    add model region

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Http\Traits\BelongsToOrganization;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
     use HasUuids, BelongsToOrganization;
+
+    protected $fillable = ['name', 'type'];
 }
