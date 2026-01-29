960ab5d36a422e4fd2d855173d0f620fecfc4a16
ujangaripin24 <ujangarifin06@gmail.com>
2026-01-29 13:38:14 +0700

    add index controller for region

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Region;
 use Illuminate\Http\Request;
-use Models\Region;
 use Inertia\Inertia;
 
 class RegionController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
-        return Inertia::render('Dashboard/RegionPage/RegionPage');
+        $query = Region::withGeoJson();
+
+        if ($request->search) {
+            $query->where('name', 'ilike', '%' . $request->search . '%');
+        }
+
+        return Inertia::render('Dashboard/RegionPage/RegionPage', [
+            'regions' => $query->latest()->paginate(10)->withQueryString(),
+            'filters' => $request->only(['search']),
+            'types' => ['province', 'city', 'district', 'custom']
+        ]);
     }
 }
