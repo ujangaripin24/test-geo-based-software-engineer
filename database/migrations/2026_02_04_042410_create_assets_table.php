46d7fda720ae11a46765aa35cac90f9284ff2524
ujangaripin24 <ujangarifin06@gmail.com>
2026-02-04 11:28:29 +0700

    code data migration assets

--- database/migrations/2026_02_04_042410_create_assets_table.php
+++ database/migrations/2026_02_04_042410_create_assets_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -12,9 +13,28 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('assets', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->foreignUuid('organization_id')
+                ->constrained('organizations')
+                ->onDelete('cascade');
+            $table->foreignUuid('region_id')
+                ->constrained('regions')
+                ->onDelete('cascade');
+            $table->string('name');
+            $table->string('category');
+            $table->string('status')->default('active');
+            $table->jsonb('meta')->nullable();
+
             $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('organization_id');
+            $table->index('region_id');
+            $table->index(['organization_id', 'category']);
         });
+        DB::statement('ALTER TABLE assets ADD COLUMN geom geometry(GEOMETRY, 4326)');
+
+        DB::statement('CREATE INDEX assets_geom_gist ON assets USING GIST (geom)');
     }
 
     /**
