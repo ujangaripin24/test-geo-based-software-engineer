73add7f003f5757a332de8193ecb826c6a5e3ed8
ujangaripin24 <ujangarifin06@gmail.com>
2026-02-04 11:35:29 +0700

    modal for assets done!

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -2,9 +2,53 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Asset extends Model
 {
-    //
+    use HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'organization_id',
+        'region_id',
+        'name',
+        'category',
+        'status',
+        'meta'
+    ];
+
+    protected $cast = [
+        'meta' => 'json'
+    ];
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
+
+    public function scopeWithGeoJson($query)
+    {
+        return $query->addSelect([
+            '*',
+            DB::raw('ST_AsGeoJSON(geom) as geojson')
+        ]);
+    }
+
+    public function scopeForOrganization($query, $organizationId)
+    {
+        return $query->where('organization_id', $organizationId);
+    }
+
+    public function scopeForRegion($query, $regionId)
+    {
+        return $query->where('region_id', $regionId);
+    }
 }
